d50292e2e60feb882e9b1556101449bfacc00281
tharindu.ranaweera <tharindu.ranaweera@syscolabs.com>
2020-06-27 09:59:34 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,74 @@
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  Button,
+} from 'react-native';
+import styles from './styles';
+import { FlatList } from 'react-native-gesture-handler';
+
+export default function Home() {
+  const [words, setWords] = useState([]);
+  const [currentWord, setCurrentWord] = useState(null);
+  const wordInputRef = useRef();
+  return (
+    <View>
+      <Text style={{ textAlign: 'center', fontSize: 14, fontWeight: '600' }}>
+        Word Bank
+      </Text>
+      <View>
+        <Text style={{ textAlign: 'center' }}>Your repo for words</Text>
+        <Image
+          source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <View style={{p:8}}>
+        <TextInput
+          style={styles.textInputView}
+          placeholder='Add a word here'
+          value={currentWord || ''}
+          onChangeText={(word) => setCurrentWord(word)}
+          ref={wordInputRef}
+        />
+      </View>
+
+      <TextInput
+        style={styles.textInputView}
+        placeholder='Definition'
+        value={currentWord || ''}
+        onChangeText={(word) => setCurrentWord(word)}
+        ref={wordInputRef}
+      />
+
+      <Button
+        onPress={() => {
+          setWords([...words, currentWord]);
+          setCurrentWord(null);
+          wordInputRef.current.clear();
+        }}
+        title='Add Word'
+      />
+
+      <Text>Added Words</Text>
+      <FlatList
+        data={words || []}
+        renderItem={({ item }) => (
+          <Text
+            style={{
+              padding: 10,
+              fontSize: 18,
+              height: 44,
+            }}
+          >
+            {item}
+          </Text>
+        )}
+      ></FlatList>
+    </View>
+  );
+}
+
+
---
+++ src/pages/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 22,
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+    textInputView:{
+        padding: 4,
+        borderRadius: 3,
+        borderWidth: 1,
+        borderColor: 'gray',
+        marginTop:4,
+        marginBottom:4,
+    }
+  });
+
+  export default styles;
